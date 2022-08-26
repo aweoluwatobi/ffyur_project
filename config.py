018340e7b41b93bf7722a12df38e225cb1fb0d7e
Oluwatobi Awe <aweoluwatobi26@gmail.com>
2022-08-26 10:48:15 +0100

    build connection string to database using environment variable values

--- config.py
+++ config.py
@@ -7,7 +7,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'support.2015')
+DB_NAME = os.getenv('DB_NAME', 'ffyur')
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:support.2015@127.0.0.1:5432/ffyur'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:support.2015@127.0.0.1:5432/ffyur'
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+SQLALCHEMY_DATABASE_URI = DB_PATH
